2fa037ad63400c965a17598e95f4f5437984d3e9
FuJ <jelkin.pavel@gmail.com>
2026-01-18 10:03:56 +0200

    Initial

---
+++ VirtualFileSystem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace VirtualFileSystem
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Initial");
+        }
+    }
+}
